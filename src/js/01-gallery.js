be3a259f61ab49dc81538c18f193350640401bc7
ArtemBlyshchyk <artem.bl31@gmail.com>
2022-10-31 19:38:17 +0200

    Prepared first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+// console.log(galleryContainer);
+const cardMarkup = galaryCardMarkup (galleryItems);
+console.log(cardMarkup);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function galaryCardMarkup (galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) =>{
+        return `
+         <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+         </a>
+        `;
+    })
+    .join('');
+};
+
+// Використовуємо функціонал  SimpleLightbox БІБЛІОТЕКИ
+let lightboxGallary = new SimpleLightbox ('.gallery a', { 
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: "250",
+});
