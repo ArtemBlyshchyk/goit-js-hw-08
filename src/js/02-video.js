e51af81214e5b4b295f496214f0e7a0ffc4c968e
ArtemBlyshchyk <artem.bl31@gmail.com>
2022-11-03 07:43:56 +0200

    Solved 2-nd Task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const keyCurrentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onSetTimeupdate, 700));
+function onSetTimeupdate(data) {
+  // console.log('data.seconds: ', data.seconds);
+  localStorage.setItem(keyCurrentTime, data.seconds);
+};
+
+if (localStorage.getItem(keyCurrentTime)) {
+  player.setCurrentTime(localStorage.getItem(keyCurrentTime));
+};
